a72d5455bc0c41cf8be34e377c7915f6fd28bfbb
Jessie <15224719330@163.com>
2019-03-19 16:09:37 +0800

    更改一个类

--- JSD1407Shoot/src/com/tarena/shoot/Airplane.java
+++ JSD1407Shoot/src/com/tarena/shoot/Airplane.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 /** 敌机:是飞行物，也是敌人 */
 public class Airplane extends FlyingObject implements Enemy {
-	private int speed = 2; //敌机移动的步数
+	private int speed = 3; //敌机移动的步数
 	
 	/** 初始化实例变量 */
 	public Airplane(){
